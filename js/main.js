e7c45ffdafc19dfd4572951d8d4d1eae7e07a102
GpanosXP <32011916+GpanosXP@users.noreply.github.com>
2017-09-16 12:42:47 +0300

    Remove milliseconds in timer
    Since most events happen in seconds, there is no need for milliseconds in the spamming timer, and so the updating interval can be extended to 1 second (from 50ms).

--- js/main.js
+++ js/main.js
@@ -7,22 +7,17 @@ const hostingSite = 'https://github.com/pRizz/iota-transaction-spammer-webapp'
 const hostingSiteTritified = iotaTransactionSpammer.tritifyURL(hostingSite)
 const significantFigures = 3
 
-function millisecondsToHHMMSSms(milliseconds) {
+function millisecondsToHHMMSS(milliseconds) {
     var sec_num = parseInt(`${milliseconds / 1000}`, 10); // don't forget the second param
     var hours   = Math.floor(sec_num / 3600);
     var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
     var seconds = sec_num - (hours * 3600) - (minutes * 60);
-    var millisecondsNum = milliseconds % 1000
 
     if (hours   < 10) {hours   = "0"+hours;}
     if (minutes < 10) {minutes = "0"+minutes;}
     if (seconds < 10) {seconds = "0"+seconds;}
-    if (millisecondsNum < 10) {
-        millisecondsNum = `00${millisecondsNum}`
-    } else if (millisecondsNum < 100) {
-        millisecondsNum = `0${millisecondsNum}`
-    }
-    return `${hours}:${minutes}:${seconds}:${millisecondsNum}`
+
+    return `${hours}:${minutes}:${seconds}`
 }
 
 $(function(){
@@ -87,14 +82,11 @@ $(function(){
         $('#confirmationsPerMinuteCount')[0].innerText = (iotaTransactionSpammer.getConfirmationCount() / durationInMinutes()).toFixed(significantFigures)
     }
     function updateTimer() {
-        $('#timeSpentSpamming')[0].innerText = millisecondsToHHMMSSms(durationInMilliseconds())
+        $('#timeSpentSpamming')[0].innerText = millisecondsToHHMMSS(durationInMilliseconds())
     }
 
     setInterval(function(){
         updateTimer()
-    }, 50)
-
-    setInterval(function(){
         updateTransactionsPerMinute()
         updateConfirmationsPerMinute()
     }, 1000)
