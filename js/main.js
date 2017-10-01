cb53bb7562cd774904467b529ae6b368b23fa03e
Peter Ryszkiewicz <pryszkie@gmail.com>
2017-09-30 22:53:57 -0500

    Fixed spamming! Made clicking on the transaction links open in new tabs instead of the current tab.

--- js/main.js
+++ js/main.js
@@ -60,13 +60,13 @@ $(function(){
         const iotaTipsBaseURL = 'http://www.iota.tips/search/?kind=transaction&hash='
         success.forEach((element) => {
             const thetangleorgURL = `${thetangleorgBaseURL}${element.hash}`
-            $('#eventLogContent').prepend(`<div>${new Date().toISOString()}: New transaction created: <a href="${thetangleorgURL}">${thetangleorgURL}</a> </div>`)
+            $('#eventLogContent').prepend(`<div>${new Date().toISOString()}: New transaction created: <a href="${thetangleorgURL}" target="_blank">${thetangleorgURL}</a></div>`)
 
             const iotaSearchURL = `${iotasearchBaseURL}${element.hash}`
-            $('#eventLogContent').prepend(`<div>${new Date().toISOString()}: New transaction created: <a href="${iotaSearchURL}">${iotaSearchURL}</a> </div>`)
+            $('#eventLogContent').prepend(`<div>${new Date().toISOString()}: New transaction created: <a href="${iotaSearchURL}" target="_blank">${iotaSearchURL}</a></div>`)
 
             const iotaTipsURL = `${iotaTipsBaseURL}${element.hash}`
-            $('#eventLogContent').prepend(`<div>${new Date().toISOString()}: New transaction created: <a href="${iotaTipsURL}">${iotaTipsURL}</a> </div>`)
+            $('#eventLogContent').prepend(`<div>${new Date().toISOString()}: New transaction created: <a href="${iotaTipsURL}" target="_blank">${iotaTipsURL}</a></div>`)
 
         })
     })
